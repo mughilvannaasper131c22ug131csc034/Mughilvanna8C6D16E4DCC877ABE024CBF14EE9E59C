8bf60fe76997caabf251c590db019720c9947299
mughilvannaasper131c22ug131csc034 <145417229+mughilvannaasper131c22ug131csc034@users.noreply.github.com>
2023-09-22 17:39:55 +0530

    Update Unit 2 challenge 2.py

--- Unit 2 challenge 2.py
+++ Unit 2 challenge 2.py
@@ -1,15 +1,22 @@
+class Player:
+
+  def play(self):
+    print("The player-playing cricket.")
+
+
 class Batsman(Player):
-    def play(self):
-        print("The batsman is batting.")
+
+  def play(self):
+    print("The batsman - batting.")
+
 
 class Bowler(Player):
-    def play(self):
-        print("The bowler is bowling.")
 
-# Create objects of Batsman and Bowler classes
+  def play(self):
+    print("The bowler- bowling.")
+
+
 batsman = Batsman()
 bowler = Bowler()
-
-# Call the play() method for each object
 batsman.play()
 bowler.play()
