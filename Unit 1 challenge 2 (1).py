f05ba048ac83c6a62eb6890ce7c4b42335615b59
mughilvannaasper131c22ug131csc034 <145417229+mughilvannaasper131c22ug131csc034@users.noreply.github.com>
2023-09-22 17:45:12 +0530

    Update Unit 1 challenge 2 (1).py

--- Unit 1 challenge 2 (1).py
+++ Unit 1 challenge 2 (1).py
@@ -1,12 +1,6 @@
- def is_leap_year(year):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        return True
-    else:
-        return False
 
-# Replace '2024' with the year you want to check
-year_to_check = 2024
-if is_leap_year(year_to_check):
-    print(year_to_check, "is a leap year.")
+year = int(input("Enter a year: "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+  print(f"{year} is a leap year.")
 else:
-    print(year_to_check, "is not a
+  print(f"{year} is not a leap year.")
